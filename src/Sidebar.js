331da4e658ebe79bb9d4c08a079db8f97415a76b
Rahul Sharma <rahulsharma15j@gmail.com>
2022-04-23 15:35:47 +0530

    initial commit

---
+++ src/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import SidebarOption from "./SidebarOption";
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <TwitterIcon />
+      <SidebarOption />
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ src/SidebarOption.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SidebarOption.css";
+
+function SidebarOption(props) {
+  return (
+    <div className="sidebarOption">
+      <h2>This is an option</h2>
+    </div>
+  );
+}
+
+export default SidebarOption;
