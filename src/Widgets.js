ec856ee75c860f3386eb23498505b573e5b9f7eb
Rahul Sharma <rahulsharma15j@gmail.com>
2022-05-12 20:47:59 +0530

    widgets section done

--- src/Widgets.js
+++ src/Widgets.js
@@ -11,12 +11,21 @@ const Widgets = () => {
   return (
     <div className="widgets">
       <div className="widgets__input">
-        <Search />
+        <Search className="widgets__searchIcon" />
         <input placeholder="Search Twitter" type="text" />
       </div>
       <div className="widgets__widgetContainer">
         <h2>What's happening?</h2>
-        <TwitterTimelineEmbed />
+        <TwitterTimelineEmbed tweetId={"1519059732086108161"} />
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="cleverqazi"
+          options={{ height: 400 }}
+        />
+        <TwitterShareButton
+          url="https://facebook.com/cleverprogrammer"
+          options={{ text: "#reactjs is awesome", via: "cleverqazi" }}
+        />
       </div>
     </div>
   );
